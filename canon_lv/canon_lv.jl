11486ce83b911047be7b72492832fcef2f4744b4
kimbochen <github@thc.anonaddy.com>
2021-08-19 14:10:24 +0800

    Fixed mathop value equality issue.

--- canon_lv/canon_lv.jl
+++ canon_lv/canon_lv.jl
@@ -9,12 +9,16 @@ end
 
 struct MathOp <: Value
     op::String
-    opd1::String
-    opd2::String
+    opr1::String
+    opr2::String
 end
 
 struct Identity <: Value
     ref_var::String
+end
+
+struct Print <: Value
+    print_var::String
 end
 
 
@@ -28,6 +32,8 @@ function canonicalizelocalvalue(instrs)
             Constant(instr)
         elseif instr["op"] == "id"
             Identity(instr, var2canon)
+        elseif instr["op"] == "print"
+            Print(instr, var2canon)
         elseif instr["op"] in math_ops
             MathOp(instr, var2canon)
         else
@@ -43,11 +49,11 @@ function Constant(instr)
 end
 
 function MathOp(instr, var2canon)
-    opd1, opd2 = [var2canon[var] for var in instr["args"]]
-    if opd1 > opd2
-        MathOp(instr["op"], opd2, opd1)
+    opr1, opr2 = (var2canon[var] for var in instr["args"])
+    if opr1 > opr2
+        MathOp(instr["op"], opr2, opr1)
     else
-        MathOp(instr["op"], opd1, opd2)
+        MathOp(instr["op"], opr1, opr2)
     end
 end
 
@@ -56,7 +62,12 @@ function Identity(instr, var2canon)
     Identity(var2canon[var])
 end
 
-function transform_instr(instr, value::Union{Constant, MathOp}, var2canon, val2canon)
+function Print(instr, var2canon)
+    var = instr["args"][1]
+    Print(var2canon[var])
+end
+
+function transform_instr(instr, value::Constant, var2canon, val2canon)
     if value in keys(val2canon)
         var = val2canon[value]
 
@@ -70,12 +81,29 @@ function transform_instr(instr, value::Union{Constant, MathOp}, var2canon, val2c
         var2canon[var] = var
         val2canon[value] = var
     end
-    instr
+end
+
+function transform_instr(instr, value::MathOp, var2canon, val2canon)
+    if value in keys(val2canon)
+        var = val2canon[value]
+        instr["op"] = "id"
+        instr["args"] = [var]
+        var2canon[instr["dest"]] = var
+    else
+        instr["args"] = [value.opr1, value.opr2]
+        var = instr["dest"]
+        var2canon[var] = var
+        val2canon[value] = var
+    end
 end
 
 function transform_instr(instr, value::Identity, var2canon, val2canon)
     instr["args"][1] = value.ref_var
     var2canon[instr["dest"]] = value.ref_var
+end
+
+function transform_instr(instr, value::Print, var2canon, val2canon)
+    instr["args"][1] = value.print_var
 end
 
 
