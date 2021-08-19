c1b65e59da8fa12905af26e560b1bc9e961501d0
kimbochen <github@thc.anonaddy.com>
2021-08-19 14:33:54 +0800

    Separated division from mathop.

--- canon_lv/canon_lv.jl
+++ canon_lv/canon_lv.jl
@@ -8,7 +8,11 @@ struct Constant <: Value
 end
 
 struct MathOp <: Value
-    op::String
+    opr1::String
+    opr2::String
+end
+
+struct DivOp <: Value
     opr1::String
     opr2::String
 end
@@ -22,39 +26,42 @@ struct Print <: Value
 end
 
 
-function canonicalizelocalvalue(instrs)
-    math_ops = ["add", "sub", "mul", "div"]
+function canonlocalvalue(instrs)
     var2canon = Dict()  # variable -> canonical variable
     val2canon = Dict()  # value -> canonical variable
+    value_method = Dict(
+        "const" => Constant,
+        "id" => Identity,
+        "print" => Print,
+        "add" => MathOp,
+        "sub" => MathOp,
+        "mul" => MathOp,
+        "div" => DivOp
+    )
 
     for instr in instrs
-        value = if instr["op"] == "const"
-            Constant(instr)
-        elseif instr["op"] == "id"
-            Identity(instr, var2canon)
-        elseif instr["op"] == "print"
-            Print(instr, var2canon)
-        elseif instr["op"] in math_ops
-            MathOp(instr, var2canon)
-        else
-            throw("Value for op '$(instr["op"])' not implemented.")
-        end
-
-        transform_instr(instr, value, var2canon, val2canon)
+        create_value = value_method[instr["op"]]
+        value = create_value(instr, var2canon)
+        canoninstr(instr, value, var2canon, val2canon)
     end
 end
 
-function Constant(instr)
+function Constant(instr, var2canon)
     Constant(instr["value"])
 end
 
 function MathOp(instr, var2canon)
     opr1, opr2 = (var2canon[var] for var in instr["args"])
     if opr1 > opr2
-        MathOp(instr["op"], opr2, opr1)
+        MathOp(opr2, opr1)
     else
-        MathOp(instr["op"], opr1, opr2)
+        MathOp(opr1, opr2)
     end
+end
+
+function DivOp(instr, var2canon)
+    opr1, opr2 = (var2canon[var] for var in instr["args"])
+    DivOp(opr1, opr2)
 end
 
 function Identity(instr, var2canon)
@@ -67,7 +74,7 @@ function Print(instr, var2canon)
     Print(var2canon[var])
 end
 
-function transform_instr(instr, value::Constant, var2canon, val2canon)
+function canoninstr(instr, value::Constant, var2canon, val2canon)
     if value in keys(val2canon)
         var = val2canon[value]
 
@@ -83,7 +90,7 @@ function transform_instr(instr, value::Constant, var2canon, val2canon)
     end
 end
 
-function transform_instr(instr, value::MathOp, var2canon, val2canon)
+function canoninstr(instr, value::Union{MathOp, DivOp}, var2canon, val2canon)
     if value in keys(val2canon)
         var = val2canon[value]
         instr["op"] = "id"
@@ -97,12 +104,12 @@ function transform_instr(instr, value::MathOp, var2canon, val2canon)
     end
 end
 
-function transform_instr(instr, value::Identity, var2canon, val2canon)
+function canoninstr(instr, value::Identity, var2canon, val2canon)
     instr["args"][1] = value.ref_var
     var2canon[instr["dest"]] = value.ref_var
 end
 
-function transform_instr(instr, value::Print, var2canon, val2canon)
+function canoninstr(instr, value::Print, var2canon, val2canon)
     instr["args"][1] = value.print_var
 end
 
@@ -110,7 +117,7 @@ end
 function main()
     prog = JSON.parse(join(readlines(stdin)))
     instrs = prog["functions"][1]["instrs"]
-    canonicalizelocalvalue(instrs)
+    canonlocalvalue(instrs)
     JSON.print(prog, 2)
 end
     
